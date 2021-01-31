a969a95acb3d1405eaba75f5d0e16fa4ea8ad633
MetaPoly“ <ksoakey@gmail.com>
2021-01-30 16:21:11 -0800

    static2

--- static2_Proj/static2/urls.py
+++ static2_Proj/static2/urls.py
@@ -1,4 +1,4 @@
-"""static1 URL Configuration
+"""static2_Proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-    
-
-from django.contrib import admin
-from django.urls import path
-from appstatic import views
+from app02 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
